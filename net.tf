6af23451b52cc63d6ad7eece65cef4d0008aba55
Anton Patsev <patsev.anton@gmail.com>
2026-05-26 10:52:59 +0600

    refactor: remove optional network variables and always create VPC

--- net.tf
+++ net.tf
@@ -1,29 +1,25 @@
 resource "yandex_vpc_network" "sentry" {
-  count     = var.create_network ? 1 : 0
   name      = "vpc"
   folder_id = local.folder_id
 }
 
 resource "yandex_vpc_subnet" "sentry-a" {
-  count          = var.create_network ? 1 : 0
   folder_id      = local.folder_id
   v4_cidr_blocks = ["10.0.1.0/24"]
   zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.sentry[0].id
+  network_id     = yandex_vpc_network.sentry.id
 }
 
 resource "yandex_vpc_subnet" "sentry-b" {
-  count          = var.create_network ? 1 : 0
   folder_id      = local.folder_id
   v4_cidr_blocks = ["10.0.2.0/24"]
   zone           = "ru-central1-b"
-  network_id     = yandex_vpc_network.sentry[0].id
+  network_id     = yandex_vpc_network.sentry.id
 }
 
 resource "yandex_vpc_subnet" "sentry-d" {
-  count          = var.create_network ? 1 : 0
   folder_id      = local.folder_id
   v4_cidr_blocks = ["10.0.3.0/24"]
   zone           = "ru-central1-d"
-  network_id     = yandex_vpc_network.sentry[0].id
+  network_id     = yandex_vpc_network.sentry.id
 }
--- variables.tf
+++ variables.tf
@@ -4,69 +4,6 @@ variable "folder_id" {
   default     = ""
 }
 
-variable "create_network" {
-  description = "Create VPC and subnets in this stack. Set false when network is provisioned by stage 1."
-  type        = bool
-  default     = true
-}
-
-variable "network_id" {
-  description = "Existing VPC network ID (required when create_network=false)"
-  type        = string
-  default     = ""
-  validation {
-    condition     = var.create_network || var.network_id != ""
-    error_message = "network_id must be set when create_network=false."
-  }
-}
-
-variable "subnet_a_id" {
-  description = "Existing subnet ID in ru-central1-a (required when create_network=false)"
-  type        = string
-  default     = ""
-  validation {
-    condition     = var.create_network || var.subnet_a_id != ""
-    error_message = "subnet_a_id must be set when create_network=false."
-  }
-}
-
-variable "subnet_b_id" {
-  description = "Existing subnet ID in ru-central1-b (required when create_network=false)"
-  type        = string
-  default     = ""
-  validation {
-    condition     = var.create_network || var.subnet_b_id != ""
-    error_message = "subnet_b_id must be set when create_network=false."
-  }
-}
-
-variable "subnet_d_id" {
-  description = "Existing subnet ID in ru-central1-d (required when create_network=false)"
-  type        = string
-  default     = ""
-  validation {
-    condition     = var.create_network || var.subnet_d_id != ""
-    error_message = "subnet_d_id must be set when create_network=false."
-  }
-}
-
-variable "subnet_a_zone" {
-  description = "Zone for subnet_a_id (required when create_network=false)"
-  type        = string
-  default     = "ru-central1-a"
-}
-
-variable "subnet_b_zone" {
-  description = "Zone for subnet_b_id (required when create_network=false)"
-  type        = string
-  default     = "ru-central1-b"
-}
-
-variable "subnet_d_zone" {
-  description = "Zone for subnet_d_id (required when create_network=false)"
-  type        = string
-  default     = "ru-central1-d"
-}
 
 variable "sentry_incluster_kafka_enabled" {
   description = "Use in-chart Kafka instead of external managed Kafka"
